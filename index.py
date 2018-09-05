93e679992da33dde767a9caff6e5a96f4658035f
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-09-05 00:37:09 -0700

    Handling /msgs/ and /msg/{msgid}

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import datetime
 import boto3
 
 dynamodb_client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
 
 def result(status, message):
     return {
@@ -15,11 +16,35 @@ def result(status, message):
     }
 
 def handler(event, context):
-    print('Received event is : '+ json.dumps(event))
-    existing_tables = dynamodb_client.list_tables()
-    data = {
-        'event': event,
-        'tables': existing_tables,
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return result(200, json.dumps(data)) 
+    # todo make this an env variable using cloudformation   
+    table = dynamodb.Table('GodMessagesTable')
+    if (event['resource'] == "/msgs"):
+        response = table.scan()
+        print('Response from scan is : ' + json.dumps(response))
+        if "ResponseMetadata" in response:
+            del response["ResponseMetadata"]
+        return {
+            'statusCode': '200',
+            'body': json.dumps(response),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    elif (event['resource'] == "/msg/{msgid}" and event['httpMethod'] == "GET"):
+        # msgid the url parameter passed to API gateway. All the URL paramteres ap pear under pathParameters
+        msgid = event['pathParameters']['msgid']
+        print('Getting the message with ID {}.'.format(msgid))
+        response = table.get_item(Key={'msgid':str(msgid)})
+        print('Response from get_item is : ' + json.dumps(response))
+        if 'Item' in response:
+            # If there is an Item key in that response then we have found the item
+            item = response['Item']
+        else:
+            # If there is no Item key in the response then we have not found the item
+            return result(400, "Msg not found")
+
+        print('Constructed result is : ' + json.dumps(item))
+        return result(200, json.dumps(item))
+    else:
+        return result(500, "The resource {} is no handled by lambda.".format(event['resource']))
