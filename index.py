6959a1cb844dd782344980399dd310ab1d05e9f2
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-09-06 21:05:42 -0700

    removed prints

--- index.py
+++ index.py
@@ -17,10 +17,8 @@ def handler(event, context):
     ddb_resource = boto3.resource('dynamodb', 'us-east-1')
     # todo make this an env variable using cloudformation   
     ddb_table = ddb_resource.Table('awscodestar-thegodproject-lambda-GodMessagesTable-183ORF0JZ5VMK')
-    
     if (event['resource'] == "/msgs"):
         response = ddb_table.scan()
-        print('Response from scan is : ' + json.dumps(response))
         if "ResponseMetadata" in response:
             del response["ResponseMetadata"]
         return {
@@ -34,17 +32,13 @@ def handler(event, context):
     elif (event['resource'] == "/msg/{msgid}" and event['httpMethod'] == "GET"):
         # msgid the url parameter passed to API gateway. All the URL paramteres ap pear under pathParameters
         msgid = event['pathParameters']['msgid']
-        print('Getting the message with ID {}.'.format(msgid))
         response = ddb_table.get_item(Key={'msgid':str(msgid)})
-        print('Response from get_item is : ' + json.dumps(response))
         if 'Item' in response:
             # If there is an Item key in that response then we have found the item
             item = response['Item']
         else:
             # If there is no Item key in the response then we have not found the item
             return result(400, "Msg not found")
-
-        print('Constructed result is : ' + json.dumps(item))
         return result(200, item)
     else:
         return result(500, "The resource {} is no handled by lambda.".format(event['resource']))
