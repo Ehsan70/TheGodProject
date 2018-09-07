f1cc30ef61571227724330f13da4eed1d8d5ce22
Ehsan Abrishami <a.ehsan70@gmail.com>
2018-09-06 21:06:24 -0700

    Cleaned up the test case for get on msgs

--- tests/test_handler_local.py
+++ tests/test_handler_local.py
@@ -10,11 +10,11 @@ class TestHandlerLocal(unittest.TestCase):
     def setUp(self):
         self.table_name = 'awscodestar-thegodproject-lambda-GodMessagesTable-183ORF0JZ5VMK'
         self.first_item = {}
-        self.first_item["msgid"] = {"S": "1" }
-        self.first_item["value"] = {"S": "Test 1" }
+        self.first_item["msgid"] = {"S": "1"}
+        self.first_item["value"] = {"S": "Test 1"}
         self.second_item = {}
-        self.second_item["msgid"] = {"S": "2" }
-        self.second_item["value"] = {"S": "Test 2" }
+        self.second_item["msgid"] = {"S": "2"}
+        self.second_item["value"] = {"S": "Test 2"}
 
     @mock_dynamodb2
     def __moto_setup(self):
@@ -41,25 +41,29 @@ class TestHandlerLocal(unittest.TestCase):
             }
         )
         print("Adding item 1: {}".format(str(self.first_item)))
-        dynamodb.put_item(TableName=self.table_name,Item=self.first_item)
+        dynamodb.put_item(TableName=self.table_name, Item=self.first_item)
         print("Adding item 2: {}".format(str(self.second_item)))
-        dynamodb.put_item(TableName=self.table_name,Item=self.second_item)
-        response = dynamodb.scan(TableName=self.table_name,)
-        print('Scan res  is ' + str(response))
+        dynamodb.put_item(TableName=self.table_name, Item=self.second_item)
 
     @mock_dynamodb2
     def test_msgs_get_event(self):
         self.__moto_setup()
-        event_dir = os.path.abspath("events/")
-        print("Reading event data from {}".format(event_dir))
-        with open(os.path.join(event_dir, 'msgs_get_event.json')) as f:
-            event_data = json.load(f)
-            result = index.handler(event_data, None)
-            print("result is " + str(result))
-            self.assertEqual(result['statusCode'], '200')
-            self.assertEqual(result['headers']
-                             ['Content-Type'], 'application/json')
-            self.assertEqual(result['body']["Count"], 2)
-            for item in result['body']['Items']:
-                self.assertEqual(item['value'], 'Test '+item['msgid'])
+        msgs_get_event_data = read_events_data('msgs_get_event.json')
+        result = index.handler(msgs_get_event_data, None)
+        print("Result of action get on resource /msgs/ is \n" + json.dumps(result, indent=2))
+        self.assertEqual(result['statusCode'], '200')
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertEqual(result['body']["Count"], 2)
+        for item in result['body']['Items']:
+            self.assertEqual(item['value'], 'Test ' + item['msgid'])
 
+
+def read_events_data(event_name):
+    '''
+    Reads the sample json event data from the events directory. 
+    '''
+    event_dir = os.path.abspath("events/")
+    print("Reading event data from {}".format(event_dir))
+    with open(os.path.join(event_dir, event_name)) as f:
+        event_data = json.load(f)
+    return event_data
